43048305152e77fa0fe4c44cce0ee90e58d35bd4
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2023-11-11 16:02:13 +0530

    First Commit in First NextJS Project

---
+++ src/app/Component1.js
@@ -0,0 +1,11 @@
+function Component1({counterdata,user})
+{
+    return(
+        <>
+        <h1>Counter is {counterdata}</h1>
+        <h1>User Name is: {user}</h1>
+        </>
+    )
+}
+
+export default Component1;
